805d81307431ba895c89d4232129e968904a78a7
IhsanNA <144515726+IhsanNA@users.noreply.github.com>
2024-05-21 23:30:55 +0700

    belum selesai

---
+++ Berita/src/App.jsx
@@ -0,0 +1,17 @@
+import Navbar from './component/Navbar' 
+import NewsBoard from './component/NewsBoard'
+
+
+  const App = () => {
+    return(
+      <div>
+        
+      <Navbar/>
+      <NewsBoard/>
+
+      
+      </div>
+    )
+  }
+
+  export default App
